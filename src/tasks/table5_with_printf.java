0f42ba5567d0b42d9e7c1089d036ab681049f536
vanda <vandana.jhinkwan@gmail.com>
2025-05-11 12:17:30 +0530

    first commit

---
+++ src/tasks/table5_with_printf.java
@@ -0,0 +1,19 @@
+package tasks;
+
+public class table5_with_printf {
+
+        public static void main(String[] args) {
+            System.out.printf("5 x 1 = %d\n", 5 * 1);
+            System.out.printf("5 x 2 = %d\n", 5 * 2);
+            System.out.printf("5 x 3 = %d\n", 5 * 3);
+            System.out.printf("5 x 4 = %d\n", 5 * 4);
+            System.out.printf("5 x 5 = %d\n", 5 * 5);
+            System.out.printf("5 x 6 = %d\n", 5 * 6);
+            System.out.printf("5 x 7 = %d\n", 5 * 7);
+            System.out.printf("5 x 8 = %d\n", 5 * 8);
+            System.out.printf("5 x 9 = %d\n", 5 * 9);
+            System.out.printf("5 x 10 = %d\n", 5 * 10);
+        }
+    }
+
+
