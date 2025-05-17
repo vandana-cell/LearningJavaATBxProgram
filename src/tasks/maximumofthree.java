d4f6af32116b64059adcb01c0ae3ec030b8071e1
vanda <vandana.jhinkwan@gmail.com>
2025-05-17 15:15:57 +0530

    Merge remote-tracking branch 'origin/main'

--- src/tasks/maximumofthree.java
+++ src/tasks/maximumofthree.java
@@ -11,3 +11,5 @@ public class maximumofthree {
 
     }
 }
+// ternary condition ? value_if_true : value_if_false;
+//nested tarnary  condition1 ? (condition2 ? value_if_true1 : value_if_false1) : (condition3 ? value_if_true2 : value_if_false2);
